e46be239a9f80710f1c0f7db1aa56cc2c22edee7
kamontat <kamontat_c@hotmail.com>
2017-04-27 11:48:58 +0700

    - Fixed size - Change color - Fixed code - Random Unit

--- src/Game.java
+++ src/Game.java
@@ -4,14 +4,14 @@ import java.util.*;
 import java.util.List;
 
 public class Game extends JFrame {
-
-	public static final int SIZE = 600;
-
+	
+	public static final int SIZE = 1000;
+	
 	private JPanel panel;
 	private List<IUnit> units = new ArrayList<IUnit>();
 	private Thread thread;
 	private boolean running;
-
+	
 	public Game() {
 		initGameData();
 		panel = new JPanel() {
@@ -26,15 +26,19 @@ public class Game extends JFrame {
 		add(panel);
 		pack();
 	}
-
+	
 	private void initGameData() {
 		for (int i = 0; i < 20; i++) {
-			Unit unit = new Unit("U" + (i + 1));
-			units.add(unit);
+			IUnit u;
+			if ((int) Math.ceil(Math.random() * 100) == 35) {
+				u = new UnitAdapter(new LegacyUnit(), 50);
+			} else {
+				u = new Unit("U" + i);
+			}
+			units.add(u);
 		}
-		// Add Legacy Units here
 	}
-
+	
 	private void start() {
 		running = true;
 		thread = new Thread() {
@@ -42,9 +46,19 @@ public class Game extends JFrame {
 			public void run() {
 				super.run();
 				while (running) {
+					for (int i = 0; i < units.size(); i++) {
+						for (int j = i + 1; j < units.size(); j++) {
+							if (units.get(i).isHit(units.get(j))) {
+								units.get(i).redirection();
+								units.get(j).redirection();
+							}
+						}
+					}
 					for (IUnit unit : units) {
 						unit.move();
 					}
+					units.removeIf(IUnit::dead);
+					if (units.isEmpty()) System.exit(0);
 					panel.repaint();
 					try {
 						Thread.sleep(1000 / 60);
@@ -56,19 +70,18 @@ public class Game extends JFrame {
 		};
 		thread.start();
 	}
-
+	
 	private void drawBackground(Graphics g) {
 		g.setColor(Color.white);
 		g.fillRect(0, 0, SIZE, SIZE);
 	}
-
+	
 	private void drawUnits(Graphics g) {
 		for (IUnit unit : units) {
-			if (unit.dead()) {
-				continue;
-			}
+			if (unit.dead()) continue;
+			
 			// Circle
-			g.setColor(Color.black);
+			g.setColor(unit.getClass().equals(UnitAdapter.class) ? Color.BLUE: Color.BLACK);
 			g.fillOval(unit.getX(), unit.getY(), Unit.SIZE, Unit.SIZE);
 			// Name
 			g.setColor(Color.gray);
@@ -78,12 +91,14 @@ public class Game extends JFrame {
 			g.drawString("HP:" + unit.getHealth(), unit.getX() - 8, unit.getY() + 25);
 		}
 	}
-
+	
 	public static void main(String[] args) {
-		Game game = new Game();
-		game.setVisible(true);
-		game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		game.start();
+		SwingUtilities.invokeLater(() -> {
+			Game game = new Game();
+			game.setVisible(true);
+			game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			game.start();
+		});
 	}
-
+	
 }
