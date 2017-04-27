bde19425762edef5e9159dd27690e3b04318a3e2
kamontat <kamontat_c@hotmail.com>
2017-04-27 11:45:59 +0700

    Prepare to decrease hp

--- src/IUnit.java
+++ src/IUnit.java
@@ -1,16 +1,20 @@
-
 public interface IUnit {
-
+	
 	public void move();
-
+	
 	public int getX();
-
+	
 	public int getY();
-
+	
 	public String getName();
-
+	
 	public int getHealth();
-
+	
 	public boolean dead();
-
+	
+	public boolean isHit(IUnit unit);
+	
+	public void decreaseHP();
+	
+	void redirection();
 }
